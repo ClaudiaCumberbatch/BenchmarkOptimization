6df4263786f716c83519c1f43f507999eb8f3e67
ClaudiaCumberbatch <12110644@mail.sustech.edu.cn>
2023-11-15 15:29:44 +0800

    add get data

--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import file_utils
 
 # 连接数据库
 def connect(database_name):
@@ -39,3 +41,23 @@ def close(conn):
     except Exception as e:
         print(f"关闭数据库连接时发生错误: {str(e)}")
         return False
+
+# 和数据库交互的全流程
+def get_HPL_data(database_name, table_name, cores, new_param):
+    try:
+        conn = connect(database_name)
+        if conn is None:
+            return None
+        result = query(conn, cores, new_param["PMAP"], new_param["SWAP"], new_param["L1"], new_param["U"], new_param["EQUIL"], new_param["DEPTH"], new_param["BCAST"], new_param["RFACT"], new_param["NDIV"], new_param["PFACT"], new_param["NBMIN"], new_param["N"], new_param["NB"], new_param["P"], new_param["Q"])
+        # 如果查询结果为空，执行搜索程序，将新结果写入数据库
+        if len(result) == 0:
+            file_utils.write_to_HPL_dat('HPL.dat', new_param, cores)
+            date = os.system("date")
+            os.system(f"mpiexec.hydra -np {cores} ./xhpl > ../result/{cores}/{date}.out 2> ../result/{cores}/{date}.err")
+            result = file_utils.parse_HPL_out(f"../result/{cores}/{date}.out")
+            store(conn, result, table_name)
+        close(conn)
+        return result
+    except Exception as e:
+        print(f"获取数据时发生错误: {str(e)}")
+        return None
