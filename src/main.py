e401a81f7524ef8a2df22f783a94d04ddde639e3
yolk-pie-L <493577282@qq.com>
2023-12-01 12:26:18 +0800

    更新结果输出工具

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import yaml
 from file_utils import *
 from output_utils import *
 from bayesian_optuna import *
+import time
 
 config = parse_config_yaml()
 
@@ -24,6 +25,9 @@ switcher = {
     "optuna_HPCG": optuna_HPCG
 }
 func = switcher.get(function_key, "Invalid choice of algorithm preference and benchmark")
-best_param, best_gflops = func(node_count, core_count, iter_count)
 
-output(iter_count, best_param, best_gflops)
+start_time = time.time()
+best_param, best_gflops = func(node_count, core_count, iter_count)
+end_time = time.time()
+
+output(iter_count, best_param, best_gflops, end_time - start_time, function_key)
--- src/output_utils.py
+++ src/output_utils.py
@@ -1,6 +1,14 @@
-def output(iter_count, param, Gflops):
+import config
+
+def output(iter_count, param, Gflops, time, function_key):
+    print("Algorithm and benchmark:", function_key)
     print("The number of iteration is: ", iter_count)
     print("The best parameter is: ")
     for key, value in param.items():
         print("{:<10} : {}".format(key, value))
     print("The best Gflops is: ", Gflops)
+    print("The total duration is ", time, "s")
+    print("The config file content is: ")
+    config_param = config.parse_config_yaml()
+    for key, value in config.items():
+        print("{:<10} : {}".format(key, value))
